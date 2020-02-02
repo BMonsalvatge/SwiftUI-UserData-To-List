51319e9961c45870c4d41540b09bbe8d80f492ec
Bryan Monsalvatge <dreadnought93@gmail.com>
2020-02-02 18:32:32 -0500

    Initial Commit

---
+++ AddToListView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AddToListView
+//
+//  Created by Bryan Monsalvatge on 2/2/20.
+//  Copyright © 2020 Bryan Monsalvatge. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
